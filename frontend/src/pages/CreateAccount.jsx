34a0aefe622ccbdf0beb9c77e29544605d688a00
ecrd03 <ecrd0321@gmail.com>
2026-04-18 13:08:56 -0700

    error issue

--- frontend/src/pages/CreateAccount.jsx
+++ frontend/src/pages/CreateAccount.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import Divider from "../components/divider"
+import Divider from "../components/Divider"
 
 export default function CreateAccount() {
     const navigate = useNavigate()
