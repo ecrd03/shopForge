dbd65b0ffd7ac1c32f69076f70fdd7c72ef1d7c8
ecrd03 <ecrd0321@gmail.com>
2026-04-20 17:00:55 -0700

    Update shopforge.jsx

--- frontend/src/pages/shopforge.jsx
+++ frontend/src/pages/shopforge.jsx
@@ -975,6 +975,7 @@ export default function ShopForge() {
                         display: "flex",
                         gap: 16,
                         alignItems: "center",
+                        flexWrap: "wrap"
                     }}
                 >
                     <input
